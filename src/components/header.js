2a5ee3e824c94df56ec07188e2ebdba81e351009
LeoSouza221 <leosouza221@gmail.com>
2023-08-03 21:32:58 -0300

    fix: adjust form and add responsive header status

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,26 @@ const headerTemplate = document.createElement('template');
 
 headerTemplate.innerHTML = `
   <style>
+    button {
+      border: 1px solid #1867c0;
+      border-radius: 0.25rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 36px;
+      font-weight: 500;
+      color: #333;
+      background-color: #1867c0;
+      padding: 0 1rem;
+      cursor: pointer;
+      text-transform: uppercase;
+      text-decoration: none;
+      text-indent: 0.09;
+      letter-spacing: 0.09em;
+      font-size: .875rem;
+      transition: color 0.5s ease;
+    }
+
     nav {
       height: 60px;
       display: flex;
@@ -31,15 +51,108 @@ headerTemplate.innerHTML = `
       padding-bottom: 5px;
       box-shadow: inset 0 -2px 0 0 #fff;
     }
+
+    #menu-desktop {
+      display: flex; 
+    }
+    #menu-mobile {
+      display: none; 
+      position: relative;
+    }
+
+    @media (max-width: 768px) {
+      ul li {
+        width: 100%;
+        padding: 4px 0;
+      }
+
+      a {
+        width: 100%;
+        display: block;
+        font-size: 1rem;
+        font-weight: normal;
+        color: rgba(0,0,0,.87);
+      }
+
+      a:hover {
+        box-shadow: none;
+        padding-bottom: 0;
+      }
+
+      #menu-desktop {
+        display: none; 
+      }
+
+      #menu-mobile {
+        display: flex; 
+        justify-content: end; c
+      }
+
+      #floating-menu {
+        display: none;
+        position: absolute;
+        bottom: -180px;
+        right: 12px;
+        width: 180px;
+        height: 165px;
+        background-color: #fff;
+        padding: 0.75rem;
+        box-shadow: 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12);
+        border-radius: 4px;
+        z-index: 1;
+      }
+
+      .floating-menu-visible {
+        display: block !important;
+      }
+
+      .menu-item {
+        padding: 0.75rem;
+      }
+
+      .menu-item a {
+        margin: 0;
+      }
+
+      .box-button {
+        padding: 0.75rem;
+      }
+    }
+
   </style>
+
   <header>
-    <nav>
+    <nav id="menu-desktop">
       <ul>
         <li><a href="/pages/button-increase.html">Button</a></li>
         <li><a href="/pages/responsive-layout.html">Responsive Layout</a></li>
         <li><a href="/pages/api-request.html">Api Request</a></li>
         <li><a href="/pages/form.html">Form</a></li>
       </ul>
+    </nav>
+    <nav id="menu-mobile">
+      <div class="box-button">
+        <button onclick="changeModalState()">
+          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="24" viewBox="0 0 28 24">
+            <path fill="white" d="M2.61 0h22.431a2.61 2.61 0 1 1 0 5.22H2.61a2.61 2.61 0 1 1 0-5.22zm0 9.39h22.431a2.61 2.61 0 1 1 0 5.22H2.61a2.61 2.61 0 1 1 0-5.22zm0 9.391h22.431a2.61 2.61 0 1 1 0 5.22H2.61a2.61 2.61 0 1 1 0-5.22z"/>
+          </svg>
+        </button>
+      </div>
+      <div id="floating-menu">
+        <div class="menu-item">
+          <a href="/pages/button-increase.html">Button</a>
+        </div>
+        <div class="menu-item">
+          <a href="/pages/responsive-layout.html">Responsive Layout</a>
+        </div>
+        <div class="menu-item">
+          <a href="/pages/api-request.html">Api Request</a>
+        </div>
+        <div class="menu-item">
+          <a href="/pages/form.html">Form</a>
+        </div>
+        </ul>
+      </div>
     </nav>
   </header>
 `;
@@ -50,7 +163,7 @@ class Header extends HTMLElement {
   }
 
   connectedCallback() {
-    const shadowRoot = this.attachShadow({ mode: 'closed' });
+    const shadowRoot = this.attachShadow({ mode: 'open' });
 
     shadowRoot.appendChild(headerTemplate.content);
   }
---
+++ src/scripts/form.js
@@ -0,0 +1,132 @@
+const form = document.querySelector('#form-validation');
+const nameElement = document.querySelector('#name');
+const emailElement = document.querySelector('#email');
+const messageElement = document.querySelector('#message');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+});
+
+form.addEventListener('input', function (event) {
+  switch (event.target.id) {
+    case 'name':
+      checkName();
+      break;
+    case 'email':
+      checkEmail();
+      break;
+    case 'message':
+      checkMessage();
+      break;
+  }
+});
+
+const errorMessages = {
+  required: "Required field",
+  notOnlySpaces: "Not only spaces",
+  minLength: (name, value) => `${name} must be at least ${value} characters long`,
+  maxLength: (name, value) => `${name} must be less than ${value} characters long`,
+  email: "Invalid email"
+};
+
+function validateEmail(email) {
+  const regex = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
+
+  return regex.test(email)
+};
+
+function validateLength(value) {
+  return !!value.length;
+};
+
+function validateMinLength(min, value) {
+  return value >= min;
+}
+
+function validateMaxLength(max, value) {
+  return value <= max;
+}
+
+function validateSpaces(value) {
+  return !!value.replace(/\s/g, '').length;
+};
+
+function hideError(input) {
+  const errorEl = input.parentElement.querySelector('.form-error');
+  
+  input.classList.remove('input-error');
+  errorEl.classList.remove('form-error-visible');
+}
+
+function showError(input, message) {
+  const errorEl = input.parentElement.querySelector('.form-error');
+  const errorMessageEl = input.parentElement.querySelector('.error-message');
+
+  input.classList.add('input-error');
+  errorEl.classList.add('form-error-visible');
+  errorMessageEl.textContent = message;
+};
+
+function checkName() {
+  let valid = false
+  const name = nameElement.value
+
+  if (!validateLength(name)) {
+    showError(nameElement, errorMessages.required);
+  } else if (!validateMinLength(3, name.length)) {
+    showError(nameElement, errorMessages.minLength('Name', 3));
+  } else if (!validateSpaces(name)) {
+    showError(nameElement, errorMessages.notOnlySpaces);
+  } else {
+    hideError(nameElement);
+    valid = true
+  }
+
+  return valid
+}
+
+function checkEmail() {
+  let valid = false
+  const email = emailElement.value
+
+  if (!validateLength(email)) {
+    showError(emailElement, errorMessages.required);
+  } else if (!validateEmail(email)) {
+    showError(emailElement, errorMessages.email);
+  } else {
+    hideError(emailElement);
+    valid = true;
+  }
+
+  return valid;
+}
+
+function checkMessage() {
+  let valid = false
+  const message = messageElement.value
+
+  if (!validateLength(message)) {
+    showError(messageElement, errorMessages.required)
+  } else if (!validateMaxLength(50, message.length)) {
+    showError(messageElement, errorMessages.maxLength('Message', 50))
+  } else {
+    hideError(messageElement);
+    valid = true
+  }
+
+  return valid
+}
+
+function validateForm() {
+  let formIsValid = false;
+
+  const nameIsValid = checkName();
+  const emailIsValid = checkEmail();
+  const messageIsValid = checkMessage();
+
+  formIsValid = nameIsValid && emailIsValid && messageIsValid;
+
+  if (formIsValid) {
+    window.alert('Success');
+  }
+};
